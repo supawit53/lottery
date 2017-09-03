71399ccef6ba7abadccafbd024f3c191b2417d1d
Moo <Moo@toffymooze>
2017-09-03 16:41:31 +0700

    ss*

--- assets/js/lottery.js
+++ assets/js/lottery.js
@@ -1,7 +1,11 @@
 /**
  * 
  */
-$( "#btnSubmit" ).click(function() {
-	//window.location.href = "home.html";
-	location.href = "home.html";
+$(document).ready(function(){
+    $('#myTable').DataTable();
+    
+    $( "#btnSubmit" ).click(function() {
+    	//window.location.href = "home.html";
+    	location.href = "home.html";
+    });
 });
