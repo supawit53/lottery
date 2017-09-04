46235354a8dcbb9c48f42aa0170d0f3514f7f5c2
Supawit.kl <supawit53@gmail.com>
2017-09-04 15:56:52 +0700

    ss*

--- assets/js/lottery.js
+++ assets/js/lottery.js
@@ -2,8 +2,6 @@
  * 
  */
 $(document).ready(function(){
-    $('#myTable').DataTable();
-    
     $( "#btnSubmit" ).click(function() {
     	//window.location.href = "home.html";
     	location.href = "home.html";
